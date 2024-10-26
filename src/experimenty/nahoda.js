dcd53ed55d10eb86c981d30ccb71542544eea0d6
Jiri Zbytovsky <zbytek@gmail.com>
2024-10-26 08:58:19 +0200

    upload src

---
+++ src/experimenty/nahoda.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+alert("Pøišel k tobì student a ptá se:Prosím vás, jaké í/ý je ve slovì v_kýø?")
+vikyr=prompt ("i/y")
+function loterie (){
+if (vikyr == "i"){
+alert("Ou, já nemám prachy, nemùžu se ti odvdìèit tak nashle!")
+}
+else{
+alert("Zaøval:KVÙLI TOBÌ JSEM DOSTAL PÌTKU  Z ÈEŠTINY TY ZRÙDO!!!")
+};}
+function nahoda (){
+  cislo = Math.round( Math.random()*2)
+  
+  if(cislo == 2){
+    alert("sdffd");
+  }
+  else if(cislo == 1){
+    alert("tohohle si nevšímej, není to dodìlané");
+  }
+  else {
+    alert("tohohle si nevšímej, není to dodìlané")
+  }
+}
+nahoda()
